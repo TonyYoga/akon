4646966a0b0c19b6a362123139e095b6514d82db
Anton Konkin <konkin.anton@gmail.com>
2017-11-30 11:50:56 +0200

    5.1.2. Создать итератор четные числа [#150]

---
+++ chapter_005/src/main/java/ru/job4j/EvenIterator.java
@@ -0,0 +1,47 @@
+package ru.job4j;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenIterator implements Iterator{
+    private final int[] values;
+    private int index;
+
+    public EvenIterator(int[] values) {
+        this.values = values;
+    }
+
+    //метод isEven возвращает true если переданное в метод число - четное
+    private boolean isEven(int value) {
+        return value%2==0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        int indexTemp = index; // временный индекс, нужен для того, чтобы не менять основной индекс
+        while (indexTemp+1<values.length) {
+            if (isEven(values[++indexTemp])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        int res;
+        do {
+            res = values[index++];
+        } while (!isEven(res)&&index<values.length);
+        if (isEven(res)) {
+            return res;
+        }
+        else throw new NoSuchElementException();
+
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
