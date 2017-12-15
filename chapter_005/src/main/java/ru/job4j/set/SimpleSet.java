8782b04dcd06d880c763f801133115c79d674284
Anton Konkin <konkin.anton@gmail.com>
2017-12-15 15:57:23 +0200

    1. Реализовать коллекцию Set на массиве [#996]

--- chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
@@ -1,4 +1,65 @@
 package ru.job4j.set;
 
-public class SimpleSet {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleSet<E> implements Iterable<E> {
+
+    private Object[] set = new Object[10];
+    private int index = 0;
+
+    boolean add(E e) {
+        if (index == 0) {
+            set[index++] = e;
+            return true;
+        } else {
+            for (int i = 0; i < index; i++) {
+                if (set[i].equals(e)) {
+                    return false;
+                }
+            }
+        }
+        if (index >= set.length) {
+            set = setGrover(set);
+        }
+        set[index++] = e;
+        return true;
+    }
+
+    private Object[] setGrover(Object[] set) {
+        Object[] tmp = new Object[set.length * 2];
+        for (int i = 0; i < set.length; i++) {
+            tmp[i] = set[i];
+        }
+        return tmp;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < set.length && set[index] != null;
+            }
+
+
+            @Override
+            public E next() {
+                if (hasNext()) {
+                    return (E) set[index++];
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+
+            }
+
+        };
+    }
 }
+
--- chapter_005/src/test/java/ru/job4j/set/SimpleSetTest.java
+++ chapter_005/src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -1,5 +1,42 @@
+package ru.job4j.set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SimpleSetTest {
+    private SimpleSet<String> ss = new SimpleSet<>();
+    @Before
+    public void setUp() {
+
+        ss.add("first");
+        ss.add("second");
+        ss.add("third");
+    }
+
+    @Test
+    public void whenAddDuplicateThenFalse() {
+        assertThat(ss.add("fourth"), is(true));
+        assertThat(ss.add("second"), is(false));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iteratorTest() {
+        Iterator it = ss.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("first"));
+        assertThat(it.next(), is("second"));
+        assertThat(it.next(), is("third"));
+        assertThat(it.next(), is("fourth"));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+
+    }
 
 }
