2b19eaf52c3b19c64f247a32a00319575d483543
Anton Konkin <konkin.anton@gmail.com>
2017-12-18 12:08:17 +0200

    3. Реализовать коллекцию типа Set на базе хэш-таблицы [#998]

--- chapter_005/src/test/java/ru/job4j/set/SimpleHashSet.java
+++ chapter_005/src/test/java/ru/job4j/set/SimpleHashSet.java
@@ -1,4 +1,56 @@
 package ru.job4j.set;
 
-public class SimpleHashSet {
+
+public class SimpleHashSet<E>{
+    private int arraylength = 100;
+    private Object[] hashtab = new Object[arraylength];
+    private int counter = 0;
+
+    private int getIndex(E e) {
+
+        return e != null ? Math.abs(e.hashCode()) % arraylength : 0;
+    }
+
+
+    public boolean add(E e) {
+
+        if (contains(e)) {
+            return false;
+        }
+        if (counter >= arraylength * 0.75) {
+            hashtab = hashTabGrove(hashtab);
+        }
+        hashtab[getIndex(e)] = e;
+        counter++;
+        return true;
+
+    }
+
+    public boolean contains(E e) {
+        return e.equals(hashtab[getIndex(e)]);
+
+    }
+
+    public boolean remove(E e) {
+        if (contains(e)) {
+            hashtab[getIndex(e)] = null;
+            counter--;
+            return true;
+        }
+        return false;
+    }
+
+    private Object[] hashTabGrove(Object[] table) {
+
+        arraylength = table.length * 2;
+        Object[] tmp = new Object[arraylength];
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                E value = (E) table[i];
+                tmp[value.hashCode()] = value;
+            }
+        }
+        return tmp;
+
+    }
 }
--- chapter_005/src/test/java/ru/job4j/set/SimpleHashSetTest.java
+++ chapter_005/src/test/java/ru/job4j/set/SimpleHashSetTest.java
@@ -1,5 +1,33 @@
+package ru.job4j.set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SimpleHashSetTest {
+    SimpleHashSet<String> shs;
+    @Before
+    public void setUp() throws Exception {
+        shs = new SimpleHashSet<>();
+    }
+
+    @Test
+    public void whenAddEuqlElementWeHaveFalse() {
+        assertThat(shs.add("first"), is(true));
+        assertThat(shs.add("second"), is(true));
+        assertThat(shs.add("third"), is(true));
+        assertThat(shs.add("fourth"), is(true));
+        assertThat(shs.add("fifth"), is(true));
+        assertThat(shs.add("fifth"), is(false));
+        assertThat(shs.contains("first"), is(true));
+        assertThat(shs.remove("first"), is(true));
+        assertThat(shs.contains("first"), is(false));
+
+
+
+
+    }
 
 }
