5f2454eb8b03774383790c6b0da3b7809a8824ad
Anton Konkin <konkin.anton@gmail.com>
2017-12-21 16:46:20 +0200

    2. Set реализованный на связном списке. [#997][#34003]

--- chapter_005/src/main/java/ru/job4j/set/SimpleLinkedSet.java
+++ chapter_005/src/main/java/ru/job4j/set/SimpleLinkedSet.java
@@ -1,66 +1,18 @@
 package ru.job4j.set;
 
+import ru.job4j.list.ContainerLinkedList;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
-public class SimpleLinkedSet<E> implements Iterable<E> {
+public class SimpleLinkedSet<E> extends ContainerLinkedList<E> {
 
-    private Node<E> root;
-    private Node<E> tail;
-
-    private class Node<E> {
-        E value;
-        Node<E> prev;
-        Node<E> next;
-
-        public Node(E value, Node<E> prev, Node<E> next) {
-            this.value = value;
-            this.prev = prev;
-            this.next = next;
+    public boolean add(E e) {
+        Iterator<E> it = iterator();
+        while (it.hasNext()) {
+            if (it.next() == e) {
+                return false;
+            }
         }
+        return super.add(e);
     }
 
-    boolean add(E e) {
-        if (root == null) {
-            root = new Node<>(e, null, null);
-            tail = root;
-        } else {
-            Node<E> tmp = root;
-            while (tmp != null) {
-                if (tmp.value.equals(e)) {
-                    return false;
-                } else {
-                    tail = tmp;
-                    tmp = tmp.next;
-                }
-            }
-            tmp = tail;
-            tail = new Node<>(e, tail, null);
-            tmp.next = tail;
-
-        }
-        return true;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return new Iterator<E>() {
-            Node<E> current = root;
-            @Override
-            public boolean hasNext() {
-
-                return current != null;
-            }
-
-            @Override
-            public E next() {
-                if (hasNext()) {
-                    E result = current.value;
-                    current = current.next;
-                    return result;
-                }
-                throw new NoSuchElementException();
-            }
-        };
-    }
 }
