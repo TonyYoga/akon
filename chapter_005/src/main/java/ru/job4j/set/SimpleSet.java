3ee884588d8c8412fd699fe5d3487895b16ee405
Anton Konkin <konkin.anton@gmail.com>
2017-12-21 16:29:05 +0200

    1. Реализовать коллекцию Set на массиве [#996][#34002]

--- chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
@@ -1,65 +1,21 @@
 package ru.job4j.set;
 
+import ru.job4j.list.DynamycList;
+
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
-public class SimpleSet<E> implements Iterable<E> {
 
-    private Object[] set = new Object[10];
-    private int index = 0;
+public class SimpleSet<E> extends DynamycList<E> {
 
-    boolean add(E e) {
-        if (index == 0) {
-            set[index++] = e;
-            return true;
-        } else {
-            for (int i = 0; i < index; i++) {
-                if (set[i].equals(e)) {
-                    return false;
-                }
+    public boolean add(E e) {
+        Iterator<E> it = iterator();
+        while (it.hasNext()) {
+            if (it.next() == e) {
+                return false;
             }
         }
-        if (index >= set.length) {
-            set = setGrover(set);
-        }
-        set[index++] = e;
+        super.add(e);
         return true;
-    }
-
-    private Object[] setGrover(Object[] set) {
-        Object[] tmp = new Object[set.length * 2];
-        for (int i = 0; i < set.length; i++) {
-            tmp[i] = set[i];
-        }
-        return tmp;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return new Iterator<E>() {
-            private int index = 0;
-
-            @Override
-            public boolean hasNext() {
-                return index < set.length && set[index] != null;
-            }
-
-
-            @Override
-            public E next() {
-                if (hasNext()) {
-                    return (E) set[index++];
-                } else {
-                    throw new NoSuchElementException();
-                }
-            }
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-
-            }
-
-        };
     }
 }
 
