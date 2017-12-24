8e7fa4f804532f465509f8aa2ac0ec38a8b9fdc0
Anton Konkin <konkin.anton@gmail.com>
2017-12-24 18:14:20 +0200

    8. Реализовать собственную структуру данных - HashMap [#1008][#35026]

--- chapter_005/src/main/java/ru/job4j/map/SimpleHashMap.java
+++ chapter_005/src/main/java/ru/job4j/map/SimpleHashMap.java
@@ -1,4 +1,144 @@
 package ru.job4j.map;
 
-public class SimpleHashMap {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleHashMap<K, V> implements Iterable {
+    private int arraylength = 100;
+    private int counter = 0;
+    private Object[] hashmap = new Object[arraylength];
+
+
+
+    public class HMentry {
+        K key;
+        V value;
+
+        public HMentry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            HMentry entry = (HMentry) o;
+
+            if (!key.equals(entry.key)) {
+                return false;
+            }
+            return value.equals(entry.value);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = key.hashCode();
+            result = 31 * result + value.hashCode();
+            return result;
+        }
+    }
+
+    //Calculating hashIndex
+
+    private int hIndex(K key) {
+        return key != null ? key.hashCode() % arraylength : 0;
+    }
+
+
+    //Method insert
+    public boolean insert(K key, V value) {
+        if (contains(key)) {
+            return false;
+        }
+        if (counter >= arraylength * 0.5) {
+            hashmap = arrayGroover(hashmap);
+        }
+        hashmap[hIndex(key)] = new HMentry(key, value);
+        counter++;
+        return true;
+    }
+
+    //Method get
+    public V get(K key) {
+        HMentry ks = (HMentry) hashmap[hIndex(key)];
+        if (ks == null) {
+            throw new NoSuchElementException();
+        }
+        return ks.value;
+    }
+
+    //method delete
+    public boolean delete(K key) {
+        if (!contains(key)) {
+            return false;
+        }
+        hashmap[hIndex(key)] = null;
+        return true;
+    }
+
+    //method contains
+    public boolean contains(K key) {
+        if (counter == 0) {
+            return false;
+        }
+        HMentry entry = (HMentry) hashmap[hIndex(key)];
+
+        return entry != null && key.equals(entry.key);
+
+    }
+
+    //privat method increasing the size of array
+    private Object[] arrayGroover(Object[] table) {
+        arraylength = table.length * 2;
+        Object[] tmp = new Object[arraylength];
+        for (Object o: table) {
+            if (o != null) {
+                HMentry ks = (HMentry) o;
+                tmp[hIndex(ks.key)] = ks;
+            }
+        }
+//        for (int i = 0; i < table.length; i++) {
+//            if (table[i] != null) {
+//                HMentry ks = (HMentry) table[i];
+//                tmp[hIndex(ks.key)] = ks;
+//            }
+//        }
+        return tmp;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            int current = 0;
+            int index = 0;
+            @Override
+            public boolean hasNext() {
+                return current < counter;
+            }
+
+            @Override
+            public Object next() {
+
+                while (hasNext()) {
+                    HMentry ks = (HMentry) hashmap[index++];
+                    if (ks != null) {
+                        current++;
+                        return ks;
+                    }
+                }
+                throw new NoSuchElementException();
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
 }
--- chapter_005/src/test/java/ru/job4j/map/SimpleHashMapTest.java
+++ chapter_005/src/test/java/ru/job4j/map/SimpleHashMapTest.java
@@ -1,5 +1,72 @@
+package ru.job4j.map;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SimpleHashMapTest {
+    private SimpleHashMap<String, String> shm;
+    private Iterator it;
 
+    @Before
+    public void setUp() {
+        shm = new SimpleHashMap<>();
+
+        
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void testInsGetDelKeySets() {
+        assertThat(shm.insert("one", "1"), is(true));
+        assertThat(shm.insert("two", "2"), is(true));
+        assertThat(shm.insert("three", "3"), is(true));
+        assertThat(shm.insert("four", "4"), is(true));
+        assertThat(shm.insert("five", "5"), is(true));
+        assertThat(shm.insert("five", "6"), is(false));
+        
+        assertThat(shm.get("one"), is("1"));
+        assertThat(shm.get("two"), is("2"));
+        assertThat(shm.get("three"), is("3"));
+        
+        assertThat(shm.delete("four"), is(true));
+        assertThat(shm.get("four"), is(false));
+        
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void testIterator() {
+        shm = new SimpleHashMap<>();
+        it = shm.iterator();
+        shm.insert("one", "1");
+        shm.insert("two", "2");
+
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        System.out.println(it.next());
+        System.out.println(it.next());
+        assertThat(it.hasNext(), is(false));
+        it.next();
+        
+    }
+
+//    @Test
+//    public void testIncreasinOfEllements() {
+//
+//        for (int i = 0; i < 51; i++) {
+//            shm.insert("el" + i, "test" + 1);
+//        }
+//        String value1 = shm.get("el10");
+//        for (int i = 0; i < 51; i++) {
+//            shm.insert("new" + i, "test" + 1);
+//        }
+//        //String value2 = shm.get("el10");
+//        //assertThat(value1.equals(value2), is(true));
+//
+//    }
 }
