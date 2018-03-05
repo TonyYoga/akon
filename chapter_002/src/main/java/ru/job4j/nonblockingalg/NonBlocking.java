ab5135659406c358fcc8b16cb2e245f3c259c9ba
Anton Konkin <konkin.anton@gmail.com>
2018-03-05 15:32:36 +0200

    1. Неблокирующий кеш [#4741]

--- chapter_002/src/main/java/ru/job4j/nonblockingalg/NonBlocking.java
+++ chapter_002/src/main/java/ru/job4j/nonblockingalg/NonBlocking.java
@@ -1,4 +1,65 @@
 package ru.job4j.nonblockingalg;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class NonBlocking {
+    private ConcurrentHashMap<Integer, Model> modelcash = new ConcurrentHashMap<>();
+    private volatile int index = 0;
+
+
+
+    static class Model {
+        String name;
+        Integer version;
+
+        public Model(String name) {
+            this.name = name;
+            this.version = 0;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+            versionUpd();
+        }
+
+        public Integer getVersion() {
+            return version;
+        }
+
+        private void setVersion(Integer version) {
+            this.version = version;
+        }
+
+        public void versionUpd() {
+            setVersion(getVersion() + 1);
+        }
+    }
+
+    public boolean add(Model model) {
+        modelcash.put(index++, model);
+        return true;
+    }
+
+    public  boolean update(int indx, Model model) {
+        if (modelcash.get(indx).getVersion() != model.getVersion()) {
+            return false; //здесь выбрасывать исключение
+
+        }
+        model.versionUpd();
+        modelcash.computeIfPresent(indx, (key, value) -> model);
+        return true;
+    }
+
+    public boolean delete(int indx) {
+        if (!modelcash.containsKey(indx)) {
+            return false;
+        }
+        modelcash.remove(indx);
+        return true;
+    }
+
 }
--- chapter_002/src/test/java/ru/job4j/nonblockingalg/NonBlockingTest.java
+++ chapter_002/src/test/java/ru/job4j/nonblockingalg/NonBlockingTest.java
@@ -1,5 +1,51 @@
+package ru.job4j.nonblockingalg;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class NonBlockingTest {
+    NonBlocking nb;
+    NonBlocking.Model mod1, mod2, mod3;
+
+    @Before
+    public void setUp() {
+        nb = new NonBlocking();
+        mod1 = new NonBlocking.Model("first");
+        mod2 = new NonBlocking.Model("second");
+        mod3 = new NonBlocking.Model("third");
+        assertThat(nb.add(mod1), is(true));
+        assertThat(nb.add(mod2), is(true));
+        assertThat(nb.add(mod3), is(true));
+
+    }
+    @Test
+    public void add() throws Exception {
+
+        assertThat(nb.add(new NonBlocking.Model("fourth")), is(true));
+
+    }
+
+    @Test
+    public void update() throws Exception {
+        NonBlocking.Model mod2upd = mod2;
+        mod2upd.setName("first1upd");
+        new Thread() {
+            @Override
+            public void run() {
+                assertThat(nb.update(1, mod2upd), is(false));
+            }
+        }.start();
+
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+        assertThat(nb.delete(2), is(true));
+
+    }
 
 }
