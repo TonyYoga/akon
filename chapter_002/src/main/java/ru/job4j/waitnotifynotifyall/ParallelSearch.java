78ffb91efd71b298e0ff692575397ddcea73a144
Anton Konkin <konkin.anton@gmail.com>
2018-03-08 15:21:51 +0200

    4. Поиск текста в файле. [#1106][#39132]

--- chapter_002/src/main/java/ru/job4j/waitnotifynotifyall/ParallelSearch.java
+++ chapter_002/src/main/java/ru/job4j/waitnotifynotifyall/ParallelSearch.java
@@ -12,9 +12,14 @@ import java.util.List;
 import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+/*
+  Параллельный поиск текста в файле
+  задача 1106
+ */
 
 @ThreadSafe
 public class ParallelSearch {
+
     private final String root;
     private final String text;
     private List<String> exts;
@@ -23,7 +28,7 @@ public class ParallelSearch {
     @GuardedBy("this")
     private final List<String> path = new ArrayList<>(); //результаты
 
-    volatile boolean finish = false;
+    private volatile boolean finish = false;
 
     public ParallelSearch(String root, String text, List<String> exts) {
         this.root = root;
@@ -31,88 +36,122 @@ public class ParallelSearch {
         this.exts = exts;
     }
 
+    class FileSearcher implements Runnable {
+
+        class Visitor extends SimpleFileVisitor {
+
+            @Override
+            public FileVisitResult preVisitDirectory(Object dir, BasicFileAttributes attrs) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Object file, BasicFileAttributes attrs) throws IOException {
+                String extension = "";
+
+                int i = file.toString().lastIndexOf(".");
+                if (i >= 1) {
+                    extension = file.toString().substring(i + 1);
+                }
+                if (extension != "" && exts.contains(extension)) {
+                    synchronized (files) {
+                        files.offer(file.toString());
+                    }
+                    changeLock(files, false);
+                }
+
+
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Object file, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Object dir, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+        }
+
+
+        @Override
+        public void run() {
+            Visitor visitor = new Visitor();
+            try {
+                Path p = Paths.get(root);
+                Files.walkFileTree(p, visitor);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finish = true;
+            //unlock and finish searching in the files
+            changeLock(files, false);
+        }
+    }
+
+    class TextSearcher implements Runnable {
+        @Override
+        public void run() {
+            Pattern pattern = Pattern.compile(text);
+
+            while (!finish) {
+                synchronized (files) {
+                    while (!files.isEmpty()) {
+                        boolean isFound = false;
+
+                        String curfile = files.poll();
+                        File file = new File(curfile);
+                        try {
+                            BufferedReader br = new BufferedReader(new FileReader(file));
+                            while (br.ready() && !isFound) {
+                                Matcher m = pattern.matcher(br.readLine());
+
+                                if (m.matches()) {
+
+                                    isFound = true;
+                                    path.add(curfile);
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                //if list of files - empty - take a pause
+                changeLock(files, true);
+            }
+        }
+    }
+
+    private void changeLock(Object sync, boolean flag) {
+        try {
+            if (flag) {
+                synchronized (sync) {
+                    //lock
+                    sync.wait();
+                }
+
+            } else {
+                synchronized (sync) {
+                    //unlock
+                    sync.notify();
+                }
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
     public void init() {
 
-        Thread search = new Thread() {
-            @Override
-            public void run() {
-                FileVisitor visitor = new FileVisitor() {
-
-                    @Override
-                    public FileVisitResult preVisitDirectory(Object dir, BasicFileAttributes attrs) throws IOException {
-                        return FileVisitResult.CONTINUE;
-                    }
-
-                    @Override
-                    public FileVisitResult visitFile(Object file, BasicFileAttributes attrs) throws IOException {
-                        String extension = "";
-
-                        int i = file.toString().lastIndexOf(".");
-                        if (i >= 1) {
-                            extension = file.toString().substring(i + 1);
-                        }
-                        if (extension != "" && exts.contains(extension)) {
-                            synchronized (files) {
-                                files.offer(file.toString());
-                            }
-                        }
-
-
-                        return FileVisitResult.CONTINUE;
-                    }
-
-                    @Override
-                    public FileVisitResult visitFileFailed(Object file, IOException exc) throws IOException {
-                        return FileVisitResult.CONTINUE;
-                    }
-
-                    @Override
-                    public FileVisitResult postVisitDirectory(Object dir, IOException exc) throws IOException {
-                        return FileVisitResult.CONTINUE;
-                    }
-
-                };
-
-                try {
-                    Path p = Paths.get(root);
-                    Files.walkFileTree(p, visitor);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                finish = true;
-            }
-        };
-        Thread read = new Thread() {
-            @Override
-            public void run() {
-                Pattern pattern = Pattern.compile(text);
-
-                while (!finish) {
-                    synchronized (files) {
-                        while (!files.isEmpty()) {
-                            boolean isFound = false;
-                            String curfile = files.poll();
-                            File file = new File(curfile);
-                            try {
-                                BufferedReader br = new BufferedReader(new FileReader(file));
-                                while (br.ready() && !isFound) {
-                                    Matcher m = pattern.matcher(br.readLine());
-
-                                    if (m.matches()) {
-
-                                        isFound = true;
-                                        path.add(curfile);
-                                    }
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-
-                        }
-                    }
-                }
-            }
-        };
+        Thread search = new Thread(new FileSearcher());
+        Thread read = new Thread(new TextSearcher());
 
         search.start();
         read.start();
